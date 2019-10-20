504c65d9a841da0098ca8331f5b3efdfc3859e55
evanSpendlove <evan.spendlove@ucdconnect.ie>
2019-10-20 08:40:34 +0100

    Identifying requirements

---
+++ user.c
@@ -0,0 +1,21 @@
+/*
+
+username: sawal   password:maskey
+username: SAGAR  password:sharma
+username: RAVI     password:agrawal
+The tasks that admin can perform in this program is listed below:
+1.Add user
+2.Delete user
+3.Edit username/password
+4.View user log
+The tasks that staff can perform in this program is listed below:
+1.Create new account
+2.Cash Deposit
+3.Cash withdrawl
+4.Fund Transfer
+5.Account information
+6.Transaction information
+7.Log out
+
+*/
+
